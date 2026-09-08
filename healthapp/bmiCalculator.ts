9fb87a7a181cd821b6166cc4ad80d4d2e8a22096
S. Dumas <me@sjdumas.comm>
2026-09-07 22:12:16 -0400

    feat: add bmi endpoint with query param validation

--- healthapp/bmiCalculator.ts
+++ healthapp/bmiCalculator.ts
@@ -1,6 +1,6 @@
 import { parseBmiArguments } from "./utils.js";
 
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
 	const heightInMeters = height / 100;
 	const bmi = weight / (heightInMeters * heightInMeters);
 
@@ -23,14 +23,16 @@ const calculateBmi = (height: number, weight: number): string => {
 	}
 };
 
-try {
-	const { height, weight } = parseBmiArguments(process.argv);
-	console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-	let errorMessage = "Something bad happened.";
-	
-	if (error instanceof Error) {
-		errorMessage += " Error: " + error.message;
+if (process.argv[1] === import.meta.filename) {
+	try {
+		const { height, weight } = parseBmiArguments(process.argv);
+		console.log(calculateBmi(height, weight));
+	} catch (error: unknown) {
+		let errorMessage = "Something bad happened.";
+
+		if (error instanceof Error) {
+			errorMessage += " Error: " + error.message;
+		}
+		console.log(errorMessage);
 	}
-	console.log(errorMessage);
 }
--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,9 +1,35 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator.ts";
 
 const app = express();
 
 app.get("/hello", (_req, res) => {
 	res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+	const { height, weight } = req.query;
+
+	if (
+		!height ||
+		!weight ||
+		typeof height !== "string" ||
+		typeof weight !== "string" ||
+		isNaN(Number(height)) ||
+		isNaN(Number(weight))
+	) {
+		return res.status(400).json( { error: "malformatted parameters" });
+	}
+
+	const heightNumber = Number(height);
+	const weightNumber = Number(weight);
+	const bmi = calculateBmi(heightNumber, weightNumber);
+
+	return res.json({
+		weight: weightNumber,
+		height: heightNumber,
+		bmi,
+	});
 });
 
 const PORT = 3003;
