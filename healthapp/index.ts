52c36b6cdb03704a207f64533aca11c84bc1abaf
S. Dumas <me@sjdumas.comm>
2026-09-07 22:44:09 -0400

    fix: update healthapp port to 3000 for e2e tests; ci: update workflow trigger branches

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -64,7 +64,7 @@ app.post("/exercises", (req, res) => {
 	return res.json(result);
 });
 
-const PORT = 3003;
+const PORT = 3000;
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT} at http://localhost:${PORT}`);
