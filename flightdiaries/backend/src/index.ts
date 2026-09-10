719419ffc27fb9eafeddcf2e355733e98b41de12
S. Dumas <me@sjdumas.comm>
2026-09-09 22:39:38 -0400

    fix: enable CORS for frontend access

--- flightdiaries/backend/src/index.ts
+++ flightdiaries/backend/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from "cors";
 import diaryRouter from './routes/diaries.ts';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3000;
